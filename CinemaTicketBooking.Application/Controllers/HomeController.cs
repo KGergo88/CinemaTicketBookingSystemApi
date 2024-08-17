5dee3316fe21c2bd63a6ad6c63436f677338ff74
Gergő Kocsis <kocsisandrasgergo@gmail.com>
2024-08-17 16:48:16 +0200

    Added the HomeController to be able to make a "hello world" test

---
+++ CinemaTicketBooking.Application/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaTicketBooking.Application.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        var htmlContent = "<h1>Cinema Ticket Booking System API</h1>";
+        return Content(htmlContent, "text/html");
+    }
+}
