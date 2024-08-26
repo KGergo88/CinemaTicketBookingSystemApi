51d91db18c6c32691be2fc902a01f571cbe70705
Gergő Kocsis <Gergoe.Kocsis@meiller.com>
2024-08-26 22:56:43 +0200

    Added Swagger support

--- CinemaTicketBooking.Web/Program.cs
+++ CinemaTicketBooking.Web/Program.cs
@@ -1,13 +1,39 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAutoMapper(typeof(CinemaTicketBooking.Application.MappingProfile));
 builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Cinema Ticket Bookin System API",
+        Description = "",
+        Version = "v1",
+        TermsOfService = null,
+        Contact = null,
+        License = new OpenApiLicense
+        {
+            Name = "MIT",
+            Url = new Uri("https://github.com/KGergo88/CinemaTicketBookingSystemApi/blob/main/license")
+        }
+    });
+
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 else
 {
