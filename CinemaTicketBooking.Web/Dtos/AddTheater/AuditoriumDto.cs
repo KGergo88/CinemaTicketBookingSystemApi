334fc4e4d61fe597bfc337b6f22c2ca6831330cc
Gergő Kocsis <Gergoe.Kocsis@meiller.com>
2024-11-06 23:08:08 +0100

    Added DTO classes for the theater and showtime management feature

---
+++ CinemaTicketBooking.Web/Dtos/AddTheater/AuditoriumDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CinemaTicketBooking.Web.Dtos.AddTheater
+{
+    public class AuditoriumDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [MinLength(1)]
+        public List<TierDto> Tiers { get; set; }
+    }
+}
---
+++ CinemaTicketBooking.Web/Dtos/AddTheater/TierDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CinemaTicketBooking.Web.Dtos.AddTheater
+{
+    public class TierDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [MinLength(1)]
+        public List<SeatDto> Seats { get; set; }
+    }
+}
