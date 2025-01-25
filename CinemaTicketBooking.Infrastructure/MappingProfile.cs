bed4604b7bbcb453b97617373709bc30435cdea6
Gergő Kocsis <Gergoe.Kocsis@meiller.com>
2025-01-25 22:02:20 +0100

    Infrastructure.MappingProfile: Removed an unnecessary comment

--- CinemaTicketBooking.Infrastructure/MappingProfile.cs
+++ CinemaTicketBooking.Infrastructure/MappingProfile.cs
@@ -6,7 +6,6 @@ public class MappingProfile : Profile
 {
     public MappingProfile()
     {
-        // Create mappings between infrastructure and domain entities
         CreateMap<Infrastructure.Entities.MovieEntity, Domain.Entities.Movie>()
             .ForMember(dest => dest.Duration, opt => opt.MapFrom(src => TimeSpan.FromSeconds(src.DurationInSeconds)))
             .ForMember(dest => dest.Genres, opt => opt.MapFrom(src => src.Genres.Select(g => g.Name)));
