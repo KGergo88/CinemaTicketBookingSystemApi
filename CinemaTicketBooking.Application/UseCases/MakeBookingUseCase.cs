ebf65816f08c6662165d14531bf8b54b555a7996
Gergő Kocsis <Gergoe.Kocsis@meiller.com>
2025-03-14 21:44:53 +0100

    MakeBookingException: Adding the innerException where possible

--- CinemaTicketBooking.Application/UseCases/MakeBookingUseCase.cs
+++ CinemaTicketBooking.Application/UseCases/MakeBookingUseCase.cs
@@ -42,7 +42,7 @@ internal class MakeBookingUseCase : IMakeBookingUseCase
         }
         catch (SeatReservationRepositoryException exception)
         {
-            throw new MakeBookingException($"Could not reserve seats! Error: \"{exception.Message}\"");
+            throw new MakeBookingException($"Could not reserve seats! Error: \"{exception.Message}\"", exception);
         }
 
         var booking = new Booking()
