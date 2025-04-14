0f2126ab15060c98f2211d412216cbd5f52fc56b
Gergő Kocsis <kocsisandrasgergo@gmail.com>
2025-04-14 22:33:19 +0200

    BookingRequestDto: Added data annotations

--- CinemaTicketBooking.Web/Dtos/MakeBooking/BookingRequestDto.cs
+++ CinemaTicketBooking.Web/Dtos/MakeBooking/BookingRequestDto.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CinemaTicketBooking.Web.Dtos.MakeBooking;
 
 public class BookingRequestDto
 {
+    [Required]
     public required Guid CustomerId { get; set; }
 
+    [Required]
     public required Guid ScreeningId { get; set; }
 
+    [MinLength(1)]
     public required List<Guid> SeatsToReserve { get; set; }
 }
