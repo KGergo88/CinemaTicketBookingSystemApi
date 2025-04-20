f7dfdfea01343f43ed07a818efd9389f3bfd634f
Gergő Kocsis <kocsisandrasgergo@gmail.com>
2025-04-20 17:34:18 +0200

    BookingRepositoryTest: Added missing access modifiers

--- CinemaTicketBooking.IntegrationTests/Infrastructure/Repositories/BookingRepositoryTest.cs
+++ CinemaTicketBooking.IntegrationTests/Infrastructure/Repositories/BookingRepositoryTest.cs
@@ -160,14 +160,14 @@ namespace CinemaTicketBooking.IntegrationTests.Infrastructure.Repositories
 
         [Theory]
         [MemberData(nameof(TimeoutUnconfirmedBookingsAsyncSetsBookingStateCorrectlyAsyncData))]
-        async Task TimeoutUnconfirmedBookingsAsyncSetsBookingStateCorrectlyAsync(int timeoutInMinutes,
-                                                                                 Theater theaterToSetup,
-                                                                                 Movie movieToSetup,
-                                                                                 Screening screeningToSetup,
-                                                                                 Pricing pricingToSetup,
-                                                                                 Customer customerToSetup,
-                                                                                 Booking bookingToSetup,
-                                                                                 BookingState expectedBookingState)
+        public async Task TimeoutUnconfirmedBookingsAsyncSetsBookingStateCorrectlyAsync(int timeoutInMinutes,
+                                                                                        Theater theaterToSetup,
+                                                                                        Movie movieToSetup,
+                                                                                        Screening screeningToSetup,
+                                                                                        Pricing pricingToSetup,
+                                                                                        Customer customerToSetup,
+                                                                                        Booking bookingToSetup,
+                                                                                        BookingState expectedBookingState)
         {
             // Arrange
             await using var db = await CreateDatabaseAsync();
@@ -197,7 +197,7 @@ namespace CinemaTicketBooking.IntegrationTests.Infrastructure.Repositories
         [Theory]
         [InlineData(-1)]
         [InlineData(0)]
-        async Task TimeoutUnconfirmedBookingsAsyncThrowsForInvalidTimeoutLimits(int timeoutInMinutes)
+        public async Task TimeoutUnconfirmedBookingsAsyncThrowsForInvalidTimeoutLimits(int timeoutInMinutes)
         {
             // Arrange
             await using var db = await CreateDatabaseAsync();
