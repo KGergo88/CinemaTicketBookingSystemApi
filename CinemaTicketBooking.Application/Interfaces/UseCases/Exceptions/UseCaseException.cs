76b7f12812c536dc7ec880f0d12d13d3288f4dbc
Gergő Kocsis <kocsisandrasgergo@gmail.com>
2025-06-23 22:20:50 +0200

    UseCaseException: Added default value for innerException

--- CinemaTicketBooking.Application/Interfaces/UseCases/Exceptions/UseCaseException.cs
+++ CinemaTicketBooking.Application/Interfaces/UseCases/Exceptions/UseCaseException.cs
@@ -1,4 +1,4 @@
 namespace CinemaTicketBooking.Application.Interfaces.UseCases.Exceptions;
 
-public class UseCaseException(string message, Exception? innerException)
+public class UseCaseException(string message, Exception? innerException = null)
     : Exception(message, innerException) { }
