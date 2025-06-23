8e16168630306feb621915937369916f9be79173
Gergő Kocsis <kocsisandrasgergo@gmail.com>
2025-06-23 17:34:09 +0200

    ISetPricingUseCase: Removed unused SetPricingUseCaseException

--- CinemaTicketBooking.Application/Interfaces/UseCases/ISetPricingUseCase.cs
+++ CinemaTicketBooking.Application/Interfaces/UseCases/ISetPricingUseCase.cs
@@ -1,10 +1,6 @@
-using CinemaTicketBooking.Application.Interfaces.UseCases.Exceptions;
 using CinemaTicketBooking.Domain.Entities;
 
 namespace CinemaTicketBooking.Application.Interfaces.UseCases;
-
-public class SetPricingUseCaseException(string message, Exception? innerException)
-    : UseCaseException(message, innerException) { }
 
 public interface ISetPricingUseCase
 {
