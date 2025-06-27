8d978634e2ed7dcb55a6327454ef28d3ae5af0a3
Gergő Kocsis <Gergoe.Kocsis@meiller.com>
2025-06-27 17:09:37 +0200

    Dropped BookingRepositoryException
    Replaced it by RrepositoryException.

--- CinemaTicketBooking.Application/Interfaces/Repositories/IBookingRepository.cs
+++ CinemaTicketBooking.Application/Interfaces/Repositories/IBookingRepository.cs
@@ -1,10 +1,6 @@
-using CinemaTicketBooking.Application.Interfaces.Repositories.Exceptions;
 using CinemaTicketBooking.Domain.Entities;
 
 namespace CinemaTicketBooking.Application.Interfaces.Repositories;
-
-public class BookingRepositoryException(string message, Exception? innerException = null)
-    : RepositoryException(message, innerException) { }
 
 public interface IBookingRepository
 {
