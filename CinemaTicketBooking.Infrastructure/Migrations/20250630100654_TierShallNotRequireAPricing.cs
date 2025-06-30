b2cf3b84a8c2a49d7f96e53b994938cb091084ff
Gergő Kocsis <kocsisandrasgergo@gmail.com>
2025-06-30 12:09:24 +0200

    Updated database migrations
    This change was somehow not migrated yet,
    and also slipped through the pending migrations test.

---
+++ CinemaTicketBooking.Infrastructure/Migrations/20250630100654_TierShallNotRequireAPricing.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaTicketBooking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class TierShallNotRequireAPricing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
