e635358530053ec9eef70cf1fa9ba7c7a691699c
Gergő Kocsis <kocsisandrasgergo@gmail.com>
2025-07-06 21:30:49 +0200

    ErrorController: Added a missing HttpXXXAttribute to the Error() action
    Swagger reported this as unambigous.

--- CinemaTicketBooking.Web/Controllers/ErrorController.cs
+++ CinemaTicketBooking.Web/Controllers/ErrorController.cs
@@ -5,6 +5,7 @@ namespace CinemaTicketBooking.Web.Controllers;
 public class ErrorController : Controller
 {
     [Route("/error")]
+    [HttpGet]
     public IActionResult Error()
     {
         var htmlContent = "<h1>Error</h1><p>An error occurred while processing your request.</p>";
