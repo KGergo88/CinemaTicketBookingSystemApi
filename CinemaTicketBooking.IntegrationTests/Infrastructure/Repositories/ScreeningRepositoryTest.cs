496c3424d93050232d89cf73e7d18e15a9228a9f
Gergő Kocsis <kocsisandrasgergo@gmail.com>
2025-07-20 18:59:47 +0200

    ScreeningRepositoryTest: Added FindScreeningIdsInTimeFrameAsyncTimeFrameOverlapCases test

--- CinemaTicketBooking.IntegrationTests/Infrastructure/Repositories/ScreeningRepositoryTest.cs
+++ CinemaTicketBooking.IntegrationTests/Infrastructure/Repositories/ScreeningRepositoryTest.cs
@@ -3,6 +3,7 @@ using CinemaTicketBooking.Application.Interfaces.Repositories.Exceptions;
 using CinemaTicketBooking.Domain.Entities;
 using CinemaTicketBooking.Infrastructure;
 using CinemaTicketBooking.Infrastructure.DatabaseSeeding;
+using CinemaTicketBooking.Infrastructure.Entities;
 using CinemaTicketBooking.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
@@ -101,6 +102,323 @@ namespace CinemaTicketBooking.IntegrationTests.Infrastructure.Repositories
             Assert.NotNull(exception);
             Assert.IsType<RepositoryException>(exception);
             Assert.Equal(exception.Message, $"{nameof(timeFrameDuration)} shall be greater than zero! Actual value: {timeFrameDuration}");
+        }
+
+        public static IEnumerable<object[]> FindScreeningIdsInTimeFrameAsyncTimeFrameOverlapCasesData()
+        {
+            // Screening ends before timeframe
+            {
+                var seedData = new DefaultSeedData();
+                var auditoriumId = seedData.Auditoriums.First().Id;
+                var movie = seedData.Movies.First();
+
+                var timeFrameStart = DateTimeOffset.Now;
+                var timeFrameDuration = TimeSpan.FromHours(2);
+
+                var screening = new ScreeningEntity
+                {
+                    Id = Guid.NewGuid(),
+                    AuditoriumId = auditoriumId,
+                    MovieId = movie.Id,
+                    Showtime = timeFrameStart - TimeSpan.FromSeconds(movie.DurationInSeconds),
+                    Language = "English",
+                };
+                seedData.Screenings.Add(screening);
+
+                var expectedFoundScreeningIds = new List<Guid>();
+
+                yield return
+                [
+                    seedData,
+                    auditoriumId,
+                    timeFrameStart,
+                    timeFrameDuration,
+                    expectedFoundScreeningIds
+                ];
+            }
+
+            // Screening ends one microsecond in the timeframe
+            {
+                var seedData = new DefaultSeedData();
+                var auditoriumId = seedData.Auditoriums.First().Id;
+                var movie = seedData.Movies.First();
+
+                var timeFrameStart = DateTimeOffset.Now;
+                var timeFrameDuration = TimeSpan.FromHours(2);
+
+                var screening = new ScreeningEntity
+                {
+                    Id = Guid.NewGuid(),
+                    AuditoriumId = auditoriumId,
+                    MovieId = movie.Id,
+                    Showtime = timeFrameStart - TimeSpan.FromSeconds(movie.DurationInSeconds) + TimeSpan.FromMicroseconds(1),
+                    Language = "English",
+                };
+                seedData.Screenings.Add(screening);
+
+                var expectedFoundScreeningIds = new List<Guid>()
+                {
+                    screening.Id
+                };
+
+                yield return
+                [
+                    seedData,
+                    auditoriumId,
+                    timeFrameStart,
+                    timeFrameDuration,
+                    expectedFoundScreeningIds
+                ];
+            }
+
+            // Screening starts and ends within the timeframe
+            {
+                var seedData = new DefaultSeedData();
+                var auditoriumId = seedData.Auditoriums.First().Id;
+                var movie = seedData.Movies.First();
+
+                var timeFrameStart = DateTimeOffset.Now;
+                var timeFrameDuration = TimeSpan.FromSeconds(movie.DurationInSeconds) + TimeSpan.FromMinutes(15);
+
+                var screening = new ScreeningEntity
+                {
+                    Id = Guid.NewGuid(),
+                    AuditoriumId = auditoriumId,
+                    MovieId = movie.Id,
+                    Showtime = timeFrameStart + TimeSpan.FromMinutes(15),
+                    Language = "English",
+                };
+                seedData.Screenings.Add(screening);
+
+                var expectedFoundScreeningIds = new List<Guid>()
+                {
+                    screening.Id
+                };
+
+                yield return
+                [
+                    seedData,
+                    auditoriumId,
+                    timeFrameStart,
+                    timeFrameDuration,
+                    expectedFoundScreeningIds
+                ];
+            }
+
+            // Screening starts one microsecond before the end of the timeframe
+            {
+                var seedData = new DefaultSeedData();
+                var auditoriumId = seedData.Auditoriums.First().Id;
+                var movie = seedData.Movies.First();
+
+                var timeFrameStart = DateTimeOffset.Now;
+                var timeFrameDuration = TimeSpan.FromHours(2);
+
+                var screening = new ScreeningEntity
+                {
+                    Id = Guid.NewGuid(),
+                    AuditoriumId = auditoriumId,
+                    MovieId = movie.Id,
+                    Showtime = timeFrameStart + timeFrameDuration - TimeSpan.FromMicroseconds(1),
+                    Language = "English",
+                };
+                seedData.Screenings.Add(screening);
+
+                var expectedFoundScreeningIds = new List<Guid>()
+                {
+                    screening.Id
+                };
+
+                yield return
+                [
+                    seedData,
+                    auditoriumId,
+                    timeFrameStart,
+                    timeFrameDuration,
+                    expectedFoundScreeningIds
+                ];
+            }
+
+            // Screening completely overlaps the timeframe
+            {
+                var seedData = new DefaultSeedData();
+                var auditoriumId = seedData.Auditoriums.First().Id;
+                var movie = seedData.Movies.First();
+
+                var timeFrameStart = DateTimeOffset.Now;
+                var timeFrameDuration = TimeSpan.FromHours(2);
+
+                var screening = new ScreeningEntity
+                {
+                    Id = Guid.NewGuid(),
+                    AuditoriumId = auditoriumId,
+                    MovieId = movie.Id,
+                    Showtime = timeFrameStart - TimeSpan.FromHours(1),
+                    Language = "English",
+                };
+                seedData.Screenings.Add(screening);
+
+                var timeFrameEnd = timeFrameStart + timeFrameDuration;
+                var timeBetweenShowtimeAndTimeFrameEnd = timeFrameEnd - screening.Showtime;
+                var movieDuration = timeBetweenShowtimeAndTimeFrameEnd + TimeSpan.FromHours(1);
+                movie.DurationInSeconds = (int)movieDuration.TotalSeconds;
+
+                var expectedFoundScreeningIds = new List<Guid>()
+                {
+                    screening.Id
+                };
+
+                yield return
+                [
+                    seedData,
+                    auditoriumId,
+                    timeFrameStart,
+                    timeFrameDuration,
+                    expectedFoundScreeningIds
+                ];
+            }
+
+            // Screening starts after the timeframe
+            {
+                var seedData = new DefaultSeedData();
+                var auditoriumId = seedData.Auditoriums.First().Id;
+                var movie = seedData.Movies.First();
+
+                var timeFrameStart = DateTimeOffset.Now;
+                var timeFrameDuration = TimeSpan.FromHours(2);
+
+                var screening = new ScreeningEntity
+                {
+                    Id = Guid.NewGuid(),
+                    AuditoriumId = auditoriumId,
+                    MovieId = movie.Id,
+                    Showtime = timeFrameStart + timeFrameDuration,
+                    Language = "English",
+                };
+                seedData.Screenings.Add(screening);
+
+                var expectedFoundScreeningIds = new List<Guid>();
+
+                yield return
+                [
+                    seedData,
+                    auditoriumId,
+                    timeFrameStart,
+                    timeFrameDuration,
+                    expectedFoundScreeningIds
+                ];
+            }
+
+            // Multiple screenings in timeframe
+            {
+                var seedData = new DefaultSeedData();
+                var auditoriumId = seedData.Auditoriums.First().Id;
+                var movie = seedData.Movies.First();
+
+                var timeFrameStart = DateTimeOffset.Now;
+                var timeFrameDuration = TimeSpan.FromHours(2);
+
+                var firstScreening = new ScreeningEntity
+                {
+                    Id = Guid.NewGuid(),
+                    AuditoriumId = auditoriumId,
+                    MovieId = movie.Id,
+                    Showtime = timeFrameStart,
+                    Language = "English",
+                };
+
+                var secondScreening = new ScreeningEntity
+                {
+                    Id = Guid.NewGuid(),
+                    AuditoriumId = auditoriumId,
+                    MovieId = movie.Id,
+                    Showtime = timeFrameStart + TimeSpan.FromMinutes(1),
+                    Language = "English",
+                };
+
+                seedData.Screenings.AddRange([firstScreening, secondScreening]);
+
+                var expectedFoundScreeningIds = new List<Guid>()
+                {
+                    firstScreening.Id,
+                    secondScreening.Id
+                };
+
+                yield return
+                [
+                    seedData,
+                    auditoriumId,
+                    timeFrameStart,
+                    timeFrameDuration,
+                    expectedFoundScreeningIds
+                ];
+            }
+
+            // Multiple screenings of which one is in timeframe
+            {
+                var seedData = new DefaultSeedData();
+                var auditoriumId = seedData.Auditoriums.First().Id;
+                var movie = seedData.Movies.First();
+
+                var timeFrameStart = DateTimeOffset.Now;
+                var timeFrameDuration = TimeSpan.FromHours(2);
+
+                var firstScreening = new ScreeningEntity
+                {
+                    Id = Guid.NewGuid(),
+                    AuditoriumId = auditoriumId,
+                    MovieId = movie.Id,
+                    Showtime = timeFrameStart  - TimeSpan.FromMinutes(movie.DurationInSeconds),
+                    Language = "English",
+                };
+
+                var secondScreening = new ScreeningEntity
+                {
+                    Id = Guid.NewGuid(),
+                    AuditoriumId = auditoriumId,
+                    MovieId = movie.Id,
+                    Showtime = timeFrameStart,
+                    Language = "English",
+                };
+
+                seedData.Screenings.AddRange([firstScreening, secondScreening]);
+
+                var expectedFoundScreeningIds = new List<Guid>()
+                {
+                    secondScreening.Id
+                };
+
+                yield return
+                [
+                    seedData,
+                    auditoriumId,
+                    timeFrameStart,
+                    timeFrameDuration,
+                    expectedFoundScreeningIds
+                ];
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(FindScreeningIdsInTimeFrameAsyncTimeFrameOverlapCasesData))]
+        async Task FindScreeningIdsInTimeFrameAsyncTimeFrameOverlapCases(SeedData seedData,
+                                                                         Guid auditoriumId,
+                                                                         DateTimeOffset timeFrameStart,
+                                                                         TimeSpan timeFrameDuration,
+                                                                         List<Guid> expectedFoundScreeningIds)
+        {
+            // Arrange
+            await using var db = await CreateDatabaseAsync(seedData);
+            var screeningRepository = new ScreeningRepository(mapper, db.Context);
+
+            // Act
+            var foundScreeningIds = await screeningRepository.FindScreeningIdsInTimeFrameAsync(auditoriumId, timeFrameStart, timeFrameDuration);
+
+            // Assert
+            Assert.NotNull(foundScreeningIds);
+            // Comparing the screening IDs without expecting that their order matches
+            Assert.Equal(expectedFoundScreeningIds.Count, foundScreeningIds.Count);
+            Assert.All(foundScreeningIds, foundScreeningId => Assert.Contains(foundScreeningId, expectedFoundScreeningIds));
         }
 
         #endregion
