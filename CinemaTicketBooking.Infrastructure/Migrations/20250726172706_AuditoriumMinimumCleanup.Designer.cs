71f3a48959a6a1991b32ebecd7aa5984077a0b1e
Gergő Kocsis <kocsisandrasgergo@gmail.com>
2025-07-26 19:28:30 +0200

    Updated migration files

--- CinemaTicketBooking.Infrastructure/Migrations/20250726172706_AuditoriumMinimumCleanup.Designer.cs
+++ CinemaTicketBooking.Infrastructure/Migrations/20250726172706_AuditoriumMinimumCleanup.Designer.cs
@@ -4,6 +4,7 @@ using CinemaTicketBooking.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CinemaTicketBooking.Infrastructure.Migrations
 {
     [DbContext(typeof(CinemaTicketBookingDbContext))]
-    partial class CinemaTicketBookingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250726172706_AuditoriumMinimumCleanup")]
+    partial class AuditoriumMinimumCleanup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CinemaTicketBooking.Infrastructure/Migrations/20250726172706_AuditoriumMinimumCleanup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaTicketBooking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AuditoriumMinimumCleanup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MinimumCleanupDurationInSeconds",
+                table: "Auditoriums",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MinimumCleanupDurationInSeconds",
+                table: "Auditoriums");
+        }
+    }
+}
